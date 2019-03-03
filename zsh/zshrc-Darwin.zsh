facab9a7fcf9bd6c758a12ffd2a46a8a66147519
Hoang Pham <hoang.unsw@gmail.com>
2019-03-04 10:02:43 +1100

    Google Cloud SDK path

--- zsh/zshrc-Darwin.zsh
+++ zsh/zshrc-Darwin.zsh
@@ -1,3 +1,8 @@
 alias ll="exa -laF --group-directories-first"
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to clipboard.'"
 
+# The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/hoang/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/hoang/google-cloud-sdk/path.zsh.inc'; fi
+
+# The next line enables shell command completion for gcloud.
+if [ -f '/Users/hoang/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/hoang/google-cloud-sdk/completion.zsh.inc'; fi
